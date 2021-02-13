e168a50eddd7ef46c8514d19f08f3cf8c85c7b6e
season <season.saw@gmail.com>
2021-02-13 10:28:23 +0800

    breeze working with uuid instead of id

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Traits\Uuids;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, Uuids;
 
     /**
      * The attributes that are mass assignable.
