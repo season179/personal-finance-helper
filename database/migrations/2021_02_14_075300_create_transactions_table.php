def0d5735d6f4ebfca33ac1e18eff8d7d253fe6d
season <season.saw@gmail.com>
2021-02-14 16:31:30 +0800

    ran artisan make model with all flag, filled up the migration for transactions table but not yet execute

---
+++ database/migrations/2021_02_14_075300_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('user_id')->constrained();
+            $table->decimal('amount');
+            $table->smallInteger('category')->default(-1);
+            $table->timestamp('transact_at')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
